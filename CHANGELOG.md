700b7cc1bc673b186f5203cccea8a83b1fa27e1f
Roberto B <roberto.butti@gmail.com>
2020-06-05 23:05:40 +0200

    link to article + count comments
    - adding comments count
    - adding link to article (url)
    - fine tuning alignment and spacing cells table

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # Changelog
 
-## 0.1.0 - WIP 202X-XX-XX
+
+## 0.1.1 - 2020-06-05
+
+- adding comments count
+- adding link to article (url)
+- fine tuning alignment and spacing cells table
+
+
+## 0.1.0 - 2020-06-04
 
 - initial release
--- resources/views/tile.blade.php
+++ resources/views/tile.blade.php
@@ -6,17 +6,19 @@
             <table class="table-auto">
                 <thead>
                     <tr>
-                        <th class="px-4 py-2">Title</th>
-                        <th class="px-4 py-2">Views</th>
-                        <th class="px-4 py-2">Reactions</th>
+                        <th class="px-2 py-2 text-xs">Title</th>
+                        <th class="px-2 py-2 text-xs">Views</th>
+                        <th class="px-2 py-2 text-xs">React</th>
+                        <th class="px-2 py-2 text-xs">Comm</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($articles as $item)
-                    <tr>
-                        <td class="border px-4 py-2">{{ $item["title"] }}</td>
-                        <td class="border px-4 py-2">{{ $item["page_views_count"] }}</td>
-                        <td class="border px-4 py-2">{{ $item["public_reactions_count"] }}</td>
+                    <tr class="hover:bg-gray-100  border-gray-200">
+                        <td class="border px-2 py-1  items-center"><a href="{{ $item["url"] }}">{{ $item["title"] }}</a></td>
+                        <td class="border px-2 py-1 text-center">{{ $item["page_views_count"] }}</td>
+                        <td class="border px-2 py-1 text-center">{{ $item["public_reactions_count"] }}</td>
+                        <td class="border px-2 py-1 text-center">{{ $item["comments_count"] }}</td>
                     </tr>
                     @endforeach
                 </tbody>
